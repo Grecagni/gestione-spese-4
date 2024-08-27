b9b521438e4366009c81732891448ceb366bce5b
Grecagni <151087457+Grecagni@users.noreply.github.com>
2024-08-27 18:27:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
+// Configurazione Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyC1vkNeOioENO-AMKGh1pw9nekabJ-WLt8",
-    authDomain: "gestione-spese-4.firebaseapp.com",
-    projectId: "gestione-spese-4",
-    storageBucket: "gestione-spese-4.appspot.com",
-    messagingSenderId: "538040125454",
-    appId: "1:538040125454:web:e43c3cd1163622ed51945f"
+  apiKey: "AIzaSyC1vkNeOioENO-AMKGh1pw9nekabJ-WLt8",
+  authDomain: "gestione-spese-4.firebaseapp.com",
+  projectId: "gestione-spese-4",
+  storageBucket: "gestione-spese-4.appspot.com",
+  messagingSenderId: "538040125454",
+  appId: "1:538040125454:web:e43c3cd1163622ed51945f"
 };
 
+// Inizializza Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
 
+// Gestione Dark Mode
 document.getElementById('dark-mode-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 });
