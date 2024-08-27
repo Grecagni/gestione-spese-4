f86e3de66a48440f4b3323ffc2b2c9b163708e1d
Grecagni <151087457+Grecagni@users.noreply.github.com>
2024-08-27 18:08:03 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,19 +1,29 @@
-// Funzione per gestire il login
 function login(email, password) {
     auth.signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
             const user = userCredential.user;
             document.getElementById('user-info').innerText = `Utente autenticato: ${user.email}`;
+            document.getElementById('login-form').style.display = 'none';
+            document.getElementById('app-content').style.display = 'block';
         })
         .catch((error) => {
             console.error("Errore durante l'autenticazione:", error.code, error.message);
         });
 }
 
-// Gestione del submit del form di login
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
+});
+
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        document.getElementById('login-form').style.display = 'none';
+        document.getElementById('app-content').style.display = 'block';
+    } else {
+        document.getElementById('login-form').style.display = 'block';
+        document.getElementById('app-content').style.display = 'none';
+    }
 });
